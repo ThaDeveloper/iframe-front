31cc8619d9dd55f0c1497b6c657f5efda5e48272
Kamya Domnick <kamya.domnick@andela.com>
2019-11-04 18:20:09 +0300

    minimise deploy errors

--- src/views/index/App.js
+++ src/views/index/App.js
@@ -1,31 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> - create react app
+
 import React, { Component } from "react";
 import logo from "../../assets/images/logo.svg";
 
 export default class Dashboard extends Component {
-<<<<<<< HEAD
-render() {
-  const { user } = this.props;
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p> Welcome
-         { user ? <span> {user.userdata.username}</span> : null}
-        </p>
-        <a>
-          DashBoard
-
-        </a>
-      </header>
-    </div>
-  );
-
-};
-
-=======
   render() {
     const { user } = this.props;
     return (
@@ -42,4 +19,3 @@ render() {
     );
   }
 }
->>>>>>> - create react app
